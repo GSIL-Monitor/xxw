b156dce10f04023cafe4f0cba2b2f2bf72b5c9b6
xxz199539 <550276107@qq.com>
2018-11-26 00:11:57 +0800

    1

--- xxw/chaos/entrypoint.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-gunicorn src.app:app -b 0.0.0.0:10160 -k meinheld.gmeinheld.MeinheldWorker -w 5
--- xxw/fraud_api/entrypoint.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-gunicorn src.main:app -b 0.0.0.0:5000 -k meinheld.gmeinheld.MeinheldWorker
